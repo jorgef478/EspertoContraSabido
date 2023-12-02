1400e0d4fc03dcfe65917ff75f30da2de116e612
jorgef478 <jorgef478@gmail.com>
2023-12-01 22:00:22 -0300

    alteração publish

--- Program.cs
+++ Program.cs
@@ -2,16 +2,22 @@
 
 Console.ForegroundColor = ConsoleColor.DarkRed;
 Console.WriteLine("~~~~~~ Pica Pau em: Esperto contra Sabido ~~~~~~");
-Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
 Console.WriteLine("Em um determinado dia na floresta, Pica Pau e Raposinha discutiam para saber como deveriam dividir sua comida...");
 Console.WriteLine("Até que Raposinha teve uma ideia de como fazer e decidiu começar a divisão da sua maneira.");
-Console.WriteLine("Para ajuda-los, você deve escrever os alimentos que serão distribuídos.");
-Console.Write("Quantos alimentos serão distribuídos? ");
+Console.WriteLine("Para ajuda-los, você deve escrever quanto de comida será distribuído a cada um.");
+Console.Write("Quantos kg de alimento serão repartidos? ");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
 int alimento = Convert.ToInt32(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.DarkBlue;
 
-if (alimento <= 0) return;
-Console.WriteLine();
-
+if (alimento <= 0)
+{
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("O NÚMERO DIGITADO É INVÁLIDO. TENTE NOVAMENTE!");
+Console.ResetColor(); Console.WriteLine();
+}
 
 int restantes = alimento;
 int picaPau = 0, raposinha = 0;
@@ -50,10 +56,10 @@ while (restantes > 0)
 
 if (paraMim == 0)
 {
-    Console.WriteLine();
+Console.WriteLine();
 }
 
 Console.WriteLine();
-Console.WriteLine($"Pica-pau recebeu {picaPau} alimentos.");
-Console.WriteLine($"Raposinha recebeu {raposinha} alimentos.");
+Console.WriteLine($"Pica-pau recebeu {picaPau} kilos de comida.");
+Console.WriteLine($"Raposinha recebeu {raposinha} kilos de comida.");
 Console.ResetColor();
